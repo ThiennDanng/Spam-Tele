17d9af31474dea036736fd013cec45f8b0a26e5e
ThiennDanng <138754965+ThiennDanng@users.noreply.github.com>
2024-01-28 15:43:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -267,4 +267,4 @@ def echo_all(message):
         bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
     except Exception as e:
         print(f"Không thể xóa tin nhắn: {e}")
-bot.polling(non_stop=True)
+bot.polling()
